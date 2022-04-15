b49b2a3b58c2a0a3ee9e894ff072785275985197
rahadi1 <r4h4d1@yahoo.com>
2022-04-15 11:43:31 +0800

    initial commit

---
+++ calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 5;
+	int b = 6;
+	int c = 5 + 6;
+	int d = 5 - 6;
+	int e = 5 * 6;
+	int f = 5 / 6;
+
+	printf("5 + 6 = %d\n", c);
+	printf("5 - 6 = %d\n", d);
+	printf("5 x 6 = %d\n", e);
+	printf("5 : 6 = %d\n", f);
+
+	return 0;
+}
