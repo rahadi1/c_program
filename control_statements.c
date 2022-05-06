3247cc030433c86bba125536a93156567ccf2213
rahadi1 <r4h4d1@yahoo.com>
2022-05-06 14:54:27 +0800

    last

--- control_statements.c
+++ control_statements.c
@@ -76,22 +76,158 @@ int main()
 		a++;
 	}
 	*/
-
-	int a,num,sum = 0,remainder;
+	/*
+	int a,b,remainder,sum = 0;
 
 	printf("Input a number: ");
+	scanf("%d", &b);
+
+	a = b;
+
+	while(b > 0){
+
+		remainder = b % 10;
+		sum += remainder;
+		b /= 10;
+	}
+
+	printf("Jumlah semua digit angka dari %d = %d\n", a, sum);
+	*/
+
+	//#################### do while loop ####################
+	/*
+	int i = 1;
+
+	do{
+
+		if(i % 3 ==0){
+
+			printf("%d\t",i);
+			
+		}
+		i++;
+
+		if(i == 100){
+
+			printf("\n");
+		}
+
+	} while(i<100);
+	*/
+	/*
+	float p, r, t;
+	char decision = 'y';
+
+	do{
+
+		printf("principal: ");
+		scanf("%f", &p);
+
+		printf("rate: ");
+		scanf("%f", &r);
+
+		printf("time: ");
+		scanf("%f", &t);
+
+		printf("Simple Interest: %.2f\n", (p * r * t) / 100);
+
+		printf("Do you need calculate SI one more time? ('y' = yes, 'n' = no) ");
+		scanf(" %c", &decision);
+	} while(decision == 'y');
+	*/
+	/*	
+	int sum = 0;
+
+	for(int i = 0; i <= 100; i++){
+
+		sum += i;
+	}
+	printf("Sum of all number is %d\n", sum);
+	*/
+	/*
+	int a;
+
+	printf("Tentukan tinggi setengah piramida: ");
 	scanf("%d", &a);
 
-	num = a;
+	for(int i = 0; i < a; i++){
 
-	while(a > 0){
+		for(int j = 0; j < i; j++){
 
-		remainder = a%10;
-		sum += remainder;
-		printf("%d\n",sum);
-		a/=10;
+			printf(" * ");
+		}
+
+		printf("\n");
 	}
-	printf("Sums of digit of %d = %d",num,sum);
+	*/
+
+	//###################### infinite loop ######################
+	/*
+	short int a = 32766;
+
+	while(a < 32768){
+
+		printf("%d\n",a);
+		a++;
+	}
+	*/
+
+	//##################### break statements #######################
+	/*
+	int i;
+
+	for(i = 0; i < 100; i++){
+
+		if(i == 77){
+
+			break;
+		}
+	}
+		printf("Value of i: %d\n", i);
+	*/
+	//#################### continue statements ########################
+
+	//print number between 0 and 10 that is not divisible by 3
+												// >>
+				//	<<
+	/*for(int i = 0; i < 10; i++){
+
+		if(i % 3 == 0){
+
+			continue;
+		}
+		printf("%d\n", i);
+	}
+	*/
+
+	//#################### switch statement ####################
+
+	int i;
+
+	printf("Input a number: ");
+	scanf("%d", &i);
+
+	switch(i){
+
+		case 1:
+			printf("Number is one.\n");
+			break;
+
+		case 2:
+			printf("Number is two.\n");
+			break;
+
+		case 3:
+			printf("Number is three.\n");
+			break;
+
+		case 4:
+			printf("Number is four.\n");
+			break;
+
+		default:
+			printf("Number is something else.\n");
+	}
 
 	return 0;
 }
